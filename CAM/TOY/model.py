0e3a62aa6b2da6cb9f1ef86b63a97a243833421a
sangheonEN <69128174+sangheonEN@users.noreply.github.com>
2021-12-10 15:05:36 +0900

    -
    -

--- CAM/TOY/model.py
+++ CAM/TOY/model.py
@@ -28,6 +28,9 @@ class CNN(nn.Module):
     def forward(self, x):
         features = self.conv(x)
         flatten = self.avg_pool(features).view(features.size(0), -1)
+        # avg_pool 후 class num에 맞게 차원이 맞춰진다고 하더라도, Fully Connected Layer를 수행해야함.
+        # 왜냐하면 avg_pool 수행한 결과는 단순히 그전 feature map에서 kernel size로 pooling 한것임. 그래서 feature를 모두 연결 지을 수 없음.
+        # 따라서, FC layer를 통해서 확보한 feature 모두를 연결시켜서 연산함.
         output = self.classifier(flatten)
         return output, features
 
